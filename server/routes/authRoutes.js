d6bdb6123368c5f1a1177cf79cf7b3337144697b
Danja <93044537+Dbln999@users.noreply.github.com>
2024-03-03 20:20:41 +0200

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const authController = require('../controller/authController')
-const router = new Router()
-
-router.post("/registration", authController.registration);
-router.post("/login", authController.login);
-
-
-module.exports = router
